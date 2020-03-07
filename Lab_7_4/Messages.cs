a4e75f36fcb1c348ca1ffe0e707c9645ed1cefde
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-03-07 17:18:31 -0600

    Add files via upload

---
+++ Lab_7_4/Messages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_7_4
+{
+    class Messages
+    {
+        public DatingProfile sender;
+
+        public string messageTitle;
+
+        public string messageData;
+
+        public bool isRead;
+        public Messages(DatingProfile sender, string messageTitle, string messageData)
+        {
+
+            this.sender = sender;
+
+            this.messageTitle = messageTitle;
+
+            this.messageData = messageData;
+
+            isRead = false;
+
+        }
+    }
+}
---
+++ Lab_7_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab_7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatingProfile bob = new DatingProfile("Bob", "Jones", 50, "Male");
+
+            bob.WriteBio("Strong outdoors type");
+
+
+
+            DatingProfile susie = new DatingProfile("Susie", "Sue", 45, "Female");
+
+            susie.WriteBio("new to this site");
+
+
+
+            bob.SendMessage("Hello Susie", "Want to get some coffee?", susie);
+
+            susie.ReadMessage();
+        }
+    }
+}
